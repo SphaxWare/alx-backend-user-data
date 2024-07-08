1de3d530b993ed6ab7fc312233b41b225e980506
Sphax <itssphax123@gmail.com>
2024-07-08 12:50:06 -0700

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,18 @@ class Auth:
     """Authentification"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentification"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorisation header"""
