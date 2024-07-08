a38c04dc3c1e5cf42c7b1ac6a3ef9f72f5431125
Sphax <itssphax123@gmail.com>
2024-07-08 12:20:53 -0700

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorise() -> str:
       - return {"error": "unauthorized"}
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbiddenise() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      forbidden
+    """
+    abort(403)
