4fde4c6505c0057941481f416e4a0e8a3e9a7fdd
Sphax <itssphax123@gmail.com>
2024-07-09 07:39:29 -0700

    fix 2.0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if (getenv("AUTH_TYPE") == auth):
+if (getenv("AUTH_TYPE") == 'auth'):
     from api.v1.auth.auth import Auth
     auth = Auth()
 
