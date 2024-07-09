f467d2d7b300ea737d3518f22ceac42b8b6d41c9
Sphax <itssphax123@gmail.com>
2024-07-09 09:54:11 -0700

    fix 1.0

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,9 @@
 """BasicAuth Class"""
 from flask import request
 from typing import List, TypeVar
+from api.vi.auth.auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """Basic Auth"""
     pass
