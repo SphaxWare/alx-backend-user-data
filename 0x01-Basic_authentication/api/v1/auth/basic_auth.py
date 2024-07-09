200a844bf7fc76cf049aa87cc89d86ccc877563b
Sphax <itssphax123@gmail.com>
2024-07-09 10:03:39 -0700

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        encoded = authorization_header.split(' ', 1)[1]
+        return encoded
