dae7ff445c686e510272b4eb481c4b3b7d52890f
Sphax <itssphax123@gmail.com>
2024-07-09 07:40:33 -0700

    fix 3.0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -49,9 +49,9 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
-        unauthorized(error)
+        abort(401)
     if auth.current_user(request) is None:
-        forbidden(error)
+        abort(403)
 
 
 if __name__ == "__main__":
