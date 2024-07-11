4461dd9eb0315350afac08d61f1a8527533b0872
Sphax <itssphax123@gmail.com>
2024-07-11 11:09:21 -0700

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,12 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self,
+                               session_id: str = None
+                               ) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
