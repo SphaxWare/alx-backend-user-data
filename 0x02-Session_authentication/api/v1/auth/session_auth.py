e17fae2c764063eb353beed66ee4d6b9fc21ea68
Sphax <itssphax123@gmail.com>
2024-07-11 11:05:41 -0700

    fix 2.0

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Session Authentication"""
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -9,11 +10,13 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """create session"""
-        if user_id is None:
+        """Creates a Session ID for a user_id"""
+
+        if user_id is None or not isinstance(user_id, str):
             return None
-        if isinstance(user_id, str):
-            return None
+
         session_id = str(uuid.uuid4())
+
         self.user_id_by_session_id[session_id] = user_id
+
         return session_id
