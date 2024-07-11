36a3ecaf381dd73f76cfb64ab7231b22c809907e
Sphax <itssphax123@gmail.com>
2024-07-11 11:39:05 -0700

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,7 +51,9 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/'
+                      ]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
@@ -59,6 +61,9 @@ def before_request() -> str:
     current_user = auth.current_user(request)
     if current_user is None:
         abort(403)
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
+        abort(401)
 
     request.current_user = current_user
 
