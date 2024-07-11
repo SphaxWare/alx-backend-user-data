cd459501e63ab533210b6ce1547fe39675701168
Sphax <itssphax123@gmail.com>
2024-07-11 11:41:44 -0700

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,7 +63,7 @@ def before_request() -> str:
         abort(403)
     if (auth.authorization_header(request) is None and
             auth.session_cookie(request) is None):
-        abort(401)
+        abort(403)
 
     request.current_user = current_user
 
