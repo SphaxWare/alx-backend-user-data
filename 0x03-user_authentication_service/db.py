39c52f3be008f37e6e0628658c0b8239360cf48e
Sphax <itssphax123@gmail.com>
2024-07-16 13:48:51 -0700

    fix importation probleme

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from typing import Dict
 from user import Base, User
 
@@ -45,11 +46,9 @@ class DB:
         """
         Find a user by specified attributes.
         """
-        session = self._session
         try:
-            user = session.query(User).filter_by(**kwargs).one()
+            return self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
-        return user
