7f152c4dfecf1708609ebbf7550deefe216c863e
Sphax <itssphax123@gmail.com>
2024-07-16 13:38:09 -0700

    fix 2.0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,8 +44,9 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """find users"""
+        session = self._session
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
+            user = session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
             raise NoResultFound()
         except InvalidRequestError:
