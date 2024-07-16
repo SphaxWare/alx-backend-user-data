30efab8af4c0108748b1a1540ae42a8eddea470c
Sphax <itssphax123@gmail.com>
2024-07-16 13:51:46 -0700

    fix 2.0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,8 +47,9 @@ class DB:
         Find a user by specified attributes.
         """
         try:
-            return self._session.query(User).filter_by(**kwargs).one()
+            user = self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
+        return user
