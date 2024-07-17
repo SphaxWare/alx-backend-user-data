e747515d72db6f94b6e1a86a770f9bc0b7b3cbbf
Sphax <itssphax123@gmail.com>
2024-07-17 10:13:31 -0700

    6. Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
