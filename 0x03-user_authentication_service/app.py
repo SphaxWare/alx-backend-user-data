517a35d9c6b7ec0e10e2a782262523e959234fc5
Sphax <itssphax123@gmail.com>
2024-07-17 18:15:48 -0700

    17. Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,18 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """
+    function to respond to the POST /reset_password route
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
