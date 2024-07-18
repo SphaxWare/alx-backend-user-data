d73533f06f085ad9332a75b5a933e1b58a77629c
Sphax <itssphax123@gmail.com>
2024-07-17 17:58:48 -0700

    15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,15 @@ def logout():
     return redirect("/")
 
 
+@app.route('/profile', methods=["GET"])
+def profile():
+    """ implement a profile function"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
